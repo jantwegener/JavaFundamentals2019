d65a298176473fa6e528b617862e0832c0178f91
Dennis <dennis.endrulat@accenture.com>
2019-10-11 08:41:06 +0200

    added method

--- team/Teamwork.java
+++ team/Teamwork.java
@@ -4,6 +4,9 @@ public class Teamwork {
     }
     
     public void methodDennis() {
+	System.out.println("Warum können Seeräuber keine Kreisflächen berechnen?");
+
+	System.out.println("Weil sie Pi raten.")
     }
     
     public void methodGiulia() {
