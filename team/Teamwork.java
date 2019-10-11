458fcb3b064555d8cf36586788a20323500a7a53
Dennis <dennis.endrulat@accenture.com>
2019-10-11 08:44:57 +0200

    fixed missing semicolon

--- team/Teamwork.java
+++ team/Teamwork.java
@@ -6,10 +6,12 @@ public class Teamwork {
     public void methodDennis() {
 	System.out.println("Warum können Seeräuber keine Kreisflächen berechnen?");
 
-	System.out.println("Weil sie Pi raten.")
+	System.out.println("Weil sie Pi raten.");
     }
     
     public void methodGiulia() {
+	int myInt = 0;
+	System.out.println("MyInt is " + i);
     }
     
     public void methodHasret() {
